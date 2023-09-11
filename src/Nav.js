1decb5ac68ce19880de481c2b12dc0fa38272ebb
Martins Oloyede <oloyedemartins15@gmail.com>
2023-09-11 22:38:05 +0100

    added all files

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { MovieContext } from './MovieContext';
+
+const Nav = () => {
+    const [movies, setMovies] = useContext(MovieContext);
+
+  return (
+    <div>
+        <h3>Martins</h3>
+        <p>List of movies: {movies.length}</p>
+    </div>
+  )
+}
+
+export default Nav;
